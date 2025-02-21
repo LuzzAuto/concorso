f79ee57e33ab26d373d56b327b07a83a337befc6
FilippoDiviesti <filippo.diviesti@studenti.unipd.it>
2025-02-21 19:17:13 +0100

    fix backtotop

--- docs/assets/js/toggle.js
+++ docs/assets/js/toggle.js
@@ -69,16 +69,3 @@ function adjustMenuOnResize() {
         });
     }
 }
-
-//BACK TO TOP
-var lastScrollTop = 0;
-window.addEventListener("scroll", function(){ 
-    var buttonBacktoTop = document.getElementById("backToTopButton");
-    var st = document.documentElement.scrollTop;
-    if (st == 0){ // || st > lastScrollTop
-        buttonBacktoTop.className = "backToTopHidden";
-    }else{
-        buttonBacktoTop.className = "backToTopShow";
-    }
-    lastScrollTop = st <= 0 ? 0 : st;
- }, false);
