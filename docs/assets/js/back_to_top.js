bb8c45e56c33c5c7adc4846243151e0b6c52db11
EM123AR <emanueleartusi03@gmail.com>
2025-02-22 17:17:14 +0100

    Cambio nome back_to_top.js

--- docs/assets/js/back_to_top.js
+++ docs/assets/js/back_to_top.js
@@ -3,7 +3,7 @@ var lastScrollTop = 0;
 window.addEventListener("scroll", function(){ 
     var buttonBacktoTop = document.getElementById("backToTopButton");
     var st = document.documentElement.scrollTop;
-    if (st == 0){ // || st > lastScrollTop
+    if (st == 0 || st > lastScrollTop){ // || st > lastScrollTop
         buttonBacktoTop.className = "backToTopHidden";
     }else{
         buttonBacktoTop.className = "backToTopShow";
