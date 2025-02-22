21a13a6074179ab958ddd589dad057cf42aaf0b8
EM123AR <emanueleartusi03@gmail.com>
2025-02-22 17:42:54 +0100

    Update back_to_top.js

--- docs/assets/js/back_to_top.js
+++ docs/assets/js/back_to_top.js
@@ -1,12 +1,18 @@
-//BACK TO TOP
 var lastScrollTop = 0;
 window.addEventListener("scroll", function(){ 
     var buttonBacktoTop = document.getElementById("backToTopButton");
     var st = document.documentElement.scrollTop;
-    if (st == 0 || st > lastScrollTop){ // || st > lastScrollTop
+    var scrollHeight = document.documentElement.scrollHeight;
+    var clientHeight = document.documentElement.clientHeight;
+
+    var isScrollingUp = st < lastScrollTop; // Controlla se si sta scorrendo verso l'alto
+    var isNearBottom = (scrollHeight - (st + clientHeight)) < 350; // Vicino alla fine della pagina
+
+    if (st == 0 || (!isScrollingUp && !isNearBottom)) {
         buttonBacktoTop.className = "backToTopHidden";
-    }else{
+    } else {
         buttonBacktoTop.className = "backToTopShow";
     }
+
     lastScrollTop = st <= 0 ? 0 : st;
- }, false);
+}, false);
